46293db08f48d29342a9b3c1e6af7aa49c59b638
Georg Ledermann <mail@georg-ledermann.de>
2018-01-05 14:05:54 +0100

    Use ActiveStorage instead of Shrine

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,6 @@
 //= require bootstrap-sprockets
 //= require corejs-typeahead/dist/typeahead.bundle
 //= require trix
-//= require blueimp-file-upload/js/vendor/jquery.ui.widget
-//= require blueimp-file-upload/js/jquery.iframe-transport
-//= require blueimp-file-upload/js/jquery.fileupload
-//= require blueimp-load-image/js/load-image
-//= require blueimp-tmpl/js/tmpl
 //= require ekko-lightbox/dist/ekko-lightbox
 //= require rails-ujs
 //= require activestorage
